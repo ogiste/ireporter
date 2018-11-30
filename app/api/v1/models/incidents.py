c49b21e59093ad16029b442de2937b7d0114233f
ogiste <alfred.opon@strathmore.edu>
2018-11-30 17:12:04 +0300

    Feature #162341932 Add endpoint for creating an incident using data structures

---
+++ app/api/v1/models/incidents.py
@@ -0,0 +1,23 @@
+incident_list = []
+
+class IncidentModel():
+    """docstring for IncidentModel."""
+    def __init__(self):
+        self.db = incident_list
+
+    def save(self,new_incident):
+        data = {
+            "id":(len(self.db)+1),
+            "createdOn": new_incident["createdOn"],
+            "createdBy":new_incident["createdBy"],
+            "title":new_incident["title"],
+            "type":new_incident["type"],
+            "location":new_incident["location"],
+            "status":new_incident["status"],
+            "images":new_incident["images"],
+            "videos":new_incident["videos"],
+            "comment":new_incident["comment"]
+        }
+
+        self.db.append(data)
+        return self.db
---
+++ app/api/v1/views/incidents.py
@@ -0,0 +1,29 @@
+import datetime
+
+from flask import Flask, make_response,jsonify,request
+from flask_restful import Resource
+
+#Local imports
+
+from app.api.v1.models import IncidentModel
+
+
+class IncidentView(Resource, IncidentModel):
+    """docstring for IncidentView."""
+
+    def __init__(self):
+        self.db = IncidentModel()
+
+    def post(self):
+        new_incident = request.get_json()
+        new_incident["createdOn"] = datetime.datetime.today().strftime('%Y-%m-%d')
+        new_incident["createdBy"] = (len(self.db.get_incidents())+1)
+        new_incident["status"] = "draft"
+        new_incident["images"] = ["/url/image1","url/image2"]
+        new_incident["videos"] = ["/url/video1","url/video2"]
+
+        incidents_data = self.db.save(new_incident)
+        return make_response(jsonify({
+            "data":incidents_data,
+            "status_code":201
+        }),201)
